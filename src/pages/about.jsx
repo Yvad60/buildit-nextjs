514d8d3fdbc7607565cfb90677afe9f209f155a4
ivad <yvesyvad@gmail.com>
2022-10-27 16:08:43 +0200

    Add title to pages

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Hero from '../components/AboutPage/Hero';
 
 export default function About() {
   return (
     <>
+      <Head>
+        <title>About</title>
+      </Head>
       <Hero />
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { FeaturedSection } from '../components/HomePage/FeaturedSection';
 import Hero from '../components/HomePage/Hero';
 import InnovativeSection from '../components/HomePage/InnovativeSection';
@@ -6,6 +7,9 @@ import OwnersSection from '../components/HomePage/OwnersSection';
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>BuildIt</title>
+      </Head>
       <Hero />
       <InnovativeSection />
       <OwnersSection />
